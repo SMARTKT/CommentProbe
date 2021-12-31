1063249368f67514c5c84c9e87e3e4298a37202b
Dewang Modi <mailtodewang@rediffmail.com>
2022-01-01 01:32:17 +0530

    Added helper script copySMKT.py to copy outputs for comment probe

---
+++ copySMKT.py
@@ -0,0 +1,25 @@
+# usage - python copySMKT.py <path to smartKT output folder> <path to required folder>
+# This code copies 
+import os
+import sys
+import shutil
+
+INFOLDER = "."    # the smartKT output folder
+OUTFOLDER = "abc"  # the folder where output will be pasted
+
+C_EXTENSION = ['c', 'C']
+CXX_EXTENSION = ['cc', 'cpp', 'cxx', 'c++']
+FILE_EXTENSION = C_EXTENSION + CXX_EXTENSION
+
+
+for root, dirs, files in os.walk(INFOLDER):
+	for fname in files:	
+		if fname.split(".")[-1] not in FILE_EXTENSION:
+		continue
+	try:
+		shutil.copyfile(os.path.join(root,fname), os.path.join(OUTFOLDER,fname))   # copying code file
+		xml_name = ".".join(fname.split(".")[:-1])+"_clang.xml"
+		shutil.copyfile(os.path.join(root,xml_name), os.path.join(OUTFOLDER,xml_name))  # copying the xml file
+	except Exception as e:
+		print("Cannot copy the files for : ",fname, e)
+
