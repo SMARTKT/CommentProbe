db6538f1a6968835aff83c584821f8cedfde08dc
Dewang Modi <32814054+dewangmodi@users.noreply.github.com>
2022-01-03 23:41:58 +0530

    Updated copySMKT.py

--- copySMKT.py
+++ copySMKT.py
@@ -4,8 +4,8 @@ import os
 import sys
 import shutil
 
-INFOLDER = "."    # the smartKT output folder
-OUTFOLDER = "abc"  # the folder where output will be pasted
+INFOLDER = sys.argv[1]    # the smartKT output folder
+OUTFOLDER = sys.argv[2]  # the folder where output will be pasted
 
 C_EXTENSION = ['c', 'C']
 CXX_EXTENSION = ['cc', 'cpp', 'cxx', 'c++']
@@ -15,11 +15,10 @@ FILE_EXTENSION = C_EXTENSION + CXX_EXTENSION
 for root, dirs, files in os.walk(INFOLDER):
 	for fname in files:	
 		if fname.split(".")[-1] not in FILE_EXTENSION:
-		continue
-	try:
-		shutil.copyfile(os.path.join(root,fname), os.path.join(OUTFOLDER,fname))   # copying code file
-		xml_name = ".".join(fname.split(".")[:-1])+"_clang.xml"
-		shutil.copyfile(os.path.join(root,xml_name), os.path.join(OUTFOLDER,xml_name))  # copying the xml file
-	except Exception as e:
-		print("Cannot copy the files for : ",fname, e)
-
+			continue
+		try:
+			shutil.copyfile(os.path.join(root,fname), os.path.join(OUTFOLDER,fname))   # copying code file
+			xml_name = ".".join(fname.split(".")[:-1])+"_clang.xml"
+			shutil.copyfile(os.path.join(root,xml_name), os.path.join(OUTFOLDER,xml_name))  # copying the xml file
+		except Exception as e:
+			print("Cannot copy the files for : ",fname, e)
