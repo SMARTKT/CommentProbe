6de5ea712806a29f31098c7b89ec90fe13e0b473
Dewang Modi <mailtodewang@rediffmail.com>
2022-01-11 14:05:27 +0530

    Updated createDotCustom.py with addition of comment ids and a limit on edge length

--- Visualization/createDotCustom.py
+++ Visualization/createDotCustom.py
@@ -19,20 +19,20 @@ New - Don't map comment ids to names
 
 
 property_mapper_path = "propertyMapper.csv" # this file has mapping from actual names of properties to names shown in graph
-ad_path = "ad.csv" # this file has list of AD concepts (strings). String matching is used to get AD relations
-prob_domain_path = "problem_domain.csv"  # this file has list of tokens and classes. String matching is used get to PROB_DOMAIN relations
-prob_domain_edge_mapper_path = "problem_edge_mapper.csv"  # this file has two columns, name of concept and name of edge. The edge name in .dot file will be taken from second column for the class name in first column
-outfile = "out_palindrome.dot" # name of the output
+ad_path = "ad_libpng.csv" # this file has list of AD concepts (strings). String matching is used to get AD relations
+prob_domain_path = "program_domain.csv"  # this file has list of tokens and classes. String matching is used get to PROB_DOMAIN relations
+prob_domain_edge_mapper_path = "program_edge_mapper.csv"  # this file has two columns, name of concept and name of edge. The edge name in .dot file will be taken from second column for the class name in first column
+outfile = "out_4symbols.dot" # name of the output
 
 symprefix = "http://smartKT/ns/symbol#"
 commprefix = "http://smartKT/ns/comment#"
 ad_name = "AD" # name of the edge with which AD concepts will be shown in graph
 ttl_file = "final.ttl"
 
-MAP_SYMBOL_IDS = False
+MAP_SYMBOL_IDS = True
 MAP_COMMENT_IDS = False
 USE_SPELLING_FOR_SYMBOLS = True # uses the spelling property for symbols as names
-CREATE_DUMMY_MAPPERS = True # if true, create mapper files for property names and AD concepts, otherwise use given files
+CREATE_DUMMY_MAPPERS = False # if true, create mapper files for property names and AD concepts, otherwise use given files
 
 
 filelist = ["pngwutil.c","pngpriv.h","png.h","pngstruct.h","pnginfo.h","pngdebug.h"]
@@ -41,13 +41,16 @@ INCLUDE_FILE_SUBSET = False # include info for files in filelist only
 SEPARATE_OUTPUTS = False  # have separate outputs for each file
 
 
-# For symbol-level visualization
+# For symbol and comment level visualization
 OUTPUT_SYMBOL_LISTS = True
-SYMBOL_ID_LIST = ["1000170889438","1103471246865"]
-USE_SYMBOL_ID_LIST = True
+SYMBOL_ID_LIST = ["1600953224222","1601602403188","1603096451228","1601776812506","1603912638530","1600002749435","1603232596633","1600037207362","2200869373071","2203606595249","2203705366872","2200526145855","2204169959852","2200574522661"]
+OUTPUT_COMMENT_LISTS = True
+COMMENT_ID_LIST = ["2190","504","2306"]
+USE_ID_LIST = True
 
 
-
+ID_LIST = SYMBOL_ID_LIST+COMMENT_ID_LIST
+EDGE_LEN_LIM = 20000
 
 import rdflib
 from rdflib import Graph
@@ -216,6 +219,17 @@ if OUTPUT_SYMBOL_LISTS is True:
     symbols_df = pd.DataFrame(columns=["Id","Filename","Text"],data=symbol_rows)
     symbols_df.to_csv("symbol_names.csv",index=None)
 
+if OUTPUT_COMMENT_LISTS is True:
+    comment_rows = []
+    for elem, filename in elem_file_map.items():
+        elem_id = remove_link_if_needed(elem,force=True)
+        if elem_id not in id_comment_token_map:
+            continue
+        comment = replace_id(elem, force_map=True)
+        comment_rows.append([elem_id,filename,name])
+    comment_df = pd.DataFrame(columns=["Id","Filename","Text"],data=comment_rows)
+    comment_df.to_csv("comment_names.csv",index=None)
+
 
 # # replacing h and t id with names
 # for r,pair_list in data_r.items():
@@ -226,7 +240,7 @@ if OUTPUT_SYMBOL_LISTS is True:
 #     data_r[r] = newlist
 
 all_names = []
-names_from_symbollist = []
+names_from_idlist = []
 if INCLUDE_FILE_SUBSET:
     if USE_SPELLING_FOR_SYMBOLS:
         for ids in symbols_to_include:
@@ -236,7 +250,7 @@ if INCLUDE_FILE_SUBSET:
                 continue
         for ids in SYMBOL_ID_LIST:
             try:
-                names_from_symbollist.append(id_spelling_map[ids])
+                names_from_idlist.append(id_spelling_map[ids])
             except KeyError:
                 continue
     else:
@@ -247,7 +261,7 @@ if INCLUDE_FILE_SUBSET:
                 continue
         for ids in SYMBOL_ID_LIST:
             try:
-                names_from_symbollist.append(id_name_map[ids])
+                names_from_idlist.append(id_name_map[ids])
             except KeyError:
                 continue        
 
@@ -256,23 +270,34 @@ if INCLUDE_FILE_SUBSET:
             all_names.append(id_comment_token_map[ids])
         except KeyError:
             continue
+
+    for ids in COMMENT_ID_LIST:
+        try:
+            names_from_idlist.append(id_comment_token_map[ids])
+        except KeyError:
+            continue
+
     all_names = list(set(all_names))
+    names_from_idlist = list(set(names_from_idlist))
 
 else:
     if USE_SPELLING_FOR_SYMBOLS:
         all_names = list(set(id_spelling_map.values()).union(set(id_comment_token_map.values())))
         for ids in SYMBOL_ID_LIST:
             try:
-                names_from_symbollist.append(id_spelling_map[ids])
+                names_from_idlist.append(id_spelling_map[ids])
             except KeyError:
                 continue  
     else:
         all_names = list(set(id_name_map.values()).union(set(id_comment_token_map.values())))
-        for ids in SYMBOL_ID_LIST:
+        for ids in COMMENT_ID_LIST:
             try:
-                names_from_symbollist.append(id_name_map[ids])
+                names_from_idlist.append(id_name_map[ids])
             except KeyError:
-                continue          
+                continue   
+    names_from_idlist = list(set(names_from_idlist))
+
+               
 
 if CREATE_DUMMY_MAPPERS:
     # creating dummy two-way mapper of properties
@@ -319,7 +344,11 @@ def add_edge_name(edge_name):
     return 'edge[label = "%s"]\n'%(escape_quote(edge_name))
 
 def add_edge(h,t):
-    return '"%s" -> "%s"\n'%(escape_quote(h),escape_quote(t))
+    edge_string = '"%s" -> "%s"\n'%(escape_quote(h),escape_quote(t))
+    if len(edge_string) < EDGE_LEN_LIM:
+        return edge_string
+    return ""
+    
 
 # constructing the .dot file data
 file_writer_map = dict()
@@ -338,14 +367,14 @@ for r in data_r.keys():
             file_writer_map[f]+= add_edge_name(prop_mapper[r])    
         for (h,t) in data_r[r]:
             if not INCLUDE_FILE_SUBSET or h in symbols_to_include or h in comments_to_include:
-                if not USE_SYMBOL_ID_LIST or (USE_SYMBOL_ID_LIST and remove_link_if_needed(h,force=True) in SYMBOL_ID_LIST):
+                if not USE_ID_LIST or (USE_ID_LIST and remove_link_if_needed(h,force=True) in ID_LIST):
                     file_writer_map[elem_file_map[h]]+= add_edge(replace_id(h),replace_id(t))
 
     else:
         outstring+= add_edge_name(prop_mapper[r])
         for (h,t) in data_r[r]:
             if not INCLUDE_FILE_SUBSET or h in symbols_to_include or h in comments_to_include:
-                if not USE_SYMBOL_ID_LIST or (USE_SYMBOL_ID_LIST and remove_link_if_needed(h,force=True) in SYMBOL_ID_LIST):
+                if not USE_ID_LIST or (USE_ID_LIST and remove_link_if_needed(h,force=True) in ID_LIST):
                     outstring+= add_edge(replace_id(h),replace_id(t))
 
 if SEPARATE_OUTPUTS:
@@ -354,10 +383,10 @@ if SEPARATE_OUTPUTS:
     for f in file_elem_map:
 
         names = None
-        if USE_SYMBOL_ID_LIST:
+        if USE_ID_LIST:
             names = []
             for i in file_elem_map[f]:
-                if i in SYMBOL_ID_LIST:
+                if i in ID_LIST:
                     names.append(replace_id(i,force_map=True))
             names = list(set(names))
         else:
@@ -372,10 +401,10 @@ if SEPARATE_OUTPUTS:
     for f in file_elem_map:
         prob_edges = {}
         names = None
-        if USE_SYMBOL_ID_LIST:
+        if USE_ID_LIST:
             names = []
             for i in file_elem_map[f]:
-                if i in SYMBOL_ID_LIST:
+                if i in ID_LIST:
                     names.append(replace_id(i,force_map=True))
             names = list(set(names))
         else:
@@ -403,16 +432,17 @@ if SEPARATE_OUTPUTS:
 
 else:
     outstring+= add_edge_name(ad_name)    
-    for h in all_names:
+    correct_list = all_names
+    if USE_ID_LIST:
+        correct_list = names_from_idlist    
+
+    for h in correct_list:
         for ad_concept in ad_list:
             if fuzzy_match(h, ad_concept):
                 outstring+= add_edge(h,ad_concept)
 
     # outstring+= add_edge_name(prob_name)    
     prob_edges = {}
-    correct_list = all_names
-    if USE_SYMBOL_ID_LIST:
-        correct_list = names_from_symbollist    
     for h in correct_list:
         for prob_domain_token in list(prob_domain_mapper.keys()):
             if fuzzy_match(h, prob_domain_token):
